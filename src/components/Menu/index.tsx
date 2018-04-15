5737f3ae228f63ed600d88dd1000f92625b835a0
Chris Loughney <chris@chrisloughney.com>
2018-04-15 11:29:57 -0700

    fix prod build issue

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -9,25 +9,18 @@ export interface MenuItem {
 	className?: string;
 }
 
-export interface MenuProps {
+export interface Props {
 	items: MenuItem[];
 	rowLength?: 1|2|3|4;
 	onItemSelect?: (item: MenuItem) => void;
 }
 
-export default function Menu({
-		items,
-		rowLength = 3,
-		onItemSelect
-	}: MenuProps): JSX.Element {
-
+const Menu: React.SFC<Props> = ({ items, rowLength = 3, onItemSelect }) => {
 	const listItemClass = `col-xs-${ 12 / rowLength }`; //TODO remove bootstrap from html
 	const menuItems = items.map((item, index) => (
 		<li key={index} className={ listItemClass }>
 			<Link to={ item.route } onClick={ () => { onItemSelect ? onItemSelect(item) : undefined; } }>
-				<div className={ `menu-item ${item.className}` }>
-					&nbsp;
-				</div>
+				<div className={ `menu-item ${item.className}` }>&nbsp;</div>
 			</Link>
 		</li>
 	));
@@ -38,3 +31,5 @@ export default function Menu({
 		</ul>
 	);
 }
+
+export default Menu;
